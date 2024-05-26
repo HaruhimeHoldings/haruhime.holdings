4f97cea3a14aca772936283d9f1da5942b28efe6
hxruhime <38597755+hxruhime@users.noreply.github.com>
2024-05-26 12:27:29 -0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { NextUIProvider } from "@nextui-org/react";
 
 import Landing            from "@container/landing/Landing";
 import Feature            from "@container/feature/Feature";
+import Footer             from "@container/footer/Footer";
 
 export default function Page() {
     return (
@@ -9,6 +10,7 @@ export default function Page() {
             <div className="flex h-screen flex-col justify-center items-center">
                 <Landing />
                 <Feature />
+                <Footer />
             </div>
         </NextUIProvider>
     );
