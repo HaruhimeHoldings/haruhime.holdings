261fdc6536200790db823b2348d3662b25be7789
hxruhime <38597755+hxruhime@users.noreply.github.com>
2024-05-26 17:47:20 -0700

    dynamic feature list

--- src/app/container/feature/Feature.tsx
+++ src/app/container/feature/Feature.tsx
@@ -1,27 +1,22 @@
-import { Badge, Image } from "@nextui-org/react"
-import { ProjectCard } from "@component/ProjectCard";
+'use client';
 
-const projects = [
-    {
-        title: "nitrous-oxi.de",
-        link: "https://nitrous-oxi.de",
-        description: "Open source OSINT tool with an exposed RESTful API and web search panel written for research purposes.",
-        imageUrl: "https://avatars1.githubusercontent.com/u/156565097",
-        language: "TypeScript / Python"
-    },
-    {
-        title: "yoshi.rest",
-        link: "https://yoshi.rest",
-        description: "Competitive fitness & lifestyle analytical platform with a web based dashboard and social integrations.",
-        imageUrl: "https://avatars1.githubusercontent.com/u/168873145",
-        language: "TypeScript"
-    }
-];
+import React, {useEffect, useState} from 'react';
+
+import { ProjectCard }              from "@component/ProjectCard";
+
+const PROJECTS_URL = "https://raw.githubusercontent.com/HaruhimeHoldings/assets/main/json/projects.json";
 
 const Feature = () => {
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        fetch(PROJECTS_URL)
+            .then((response) => response.json())
+            .then((data) => setProjects(data));
+    }, []);
+
     return (
         <div>
-            {/* Projects Grid (2 wide)*/}
             <div className="grid grid-cols-2 gap-4">
                 {projects.map((project, index) => (
                     <ProjectCard key={index} project={project} />
